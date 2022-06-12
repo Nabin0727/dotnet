229958c274fd9ffeaed1fff6f8ed5dbb44b741a5
Nabin Lopchan <lopchannabeen138@gmail.com>
2022-06-12 22:59:16 +0545

    initial commit

---
+++ ModelBinding/ModelBinding/Models/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ModelBinding.Models
+{
+    public class Employee
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(15)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(10, 30)]
+        public int Age { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Url]
+        public string Url { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male, Female, Others
+    }
+}
